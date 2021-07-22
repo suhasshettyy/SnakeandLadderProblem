38cd76ee6ed2410cf81073639e5ed0405ac436a2
suhasshettyy <scshetty07@gmail.com>
2021-07-22 20:04:42 +0530

    [Suhas] Add: UC4-salWinposition

--- SnakeandLadderProblem/snakeandladder.cs
+++ SnakeandLadderProblem/snakeandladder.cs
@@ -6,35 +6,38 @@ namespace SnakeandLadderProblem
 {
     class Snakeandladder
     {
-        
+
         public int position = 0;
         public void Dice()
         {
-            Console.WriteLine("The current position of the player is = " + position);
-            Random random = new Random();
-            int dice = random.Next(1, 7);
-            Console.WriteLine("Then the player got in dice : " + dice);
-            int choice = random.Next(1, 3);
-            switch (choice)
+            while (position < 100)
             {
-                case 1:
-                    Console.WriteLine("The player got ladder");
-                    position += dice;
-                    Console.WriteLine("The position of player after dice = " + position);
-                    break;
-                case 2:
-                    Console.WriteLine("The player got snake");
-                    position -= dice;
-                    if (position < 0)
-                        position = 0;
-                    Console.WriteLine("The position of player after dice = " + position);
-                    break;
-                default:
-                    Console.WriteLine("The player got nothing ");
-                    Console.WriteLine("The position of player after dice = " + position);
-                    break;
+                Console.WriteLine("The current position of the player is = " + position);
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine("Then the player got in dice : " + dice);
+                int choice = random.Next(1, 3);
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("The player got ladder");
+                        position += dice;
+                        Console.WriteLine("The position of player after dice = " + position);
+                        break;
+                    case 2:
+                        Console.WriteLine("The player got snake");
+                        position -= dice;
+                        if (position < 0)
+                            position = 0;
+                        Console.WriteLine("The position of player after dice = " + position);
+                        break;
+                    default:
+                        Console.WriteLine("The player got nothing ");
+                        Console.WriteLine("The position of player after dice = " + position);
+                        break;
+                }
             }
-        }
 
+        }
     }
 }
